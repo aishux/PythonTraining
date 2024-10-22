996c8b58d84bb3d198483aa31b0a080f77cc7cb6
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-22 20:58:14 +0530

    Create 3.2 Comparison Operators.py

---
+++ 3.2 Comparison Operators.py
@@ -0,0 +1,24 @@
+## 3.2 Comparison Operators
+
+# Comparison operators compare two values and return True or False depending on the condition.
+# These operators are often used in conditional statements to make decisions in your program.
+
+# Equal to
+x = 5
+y = 3
+print(x == y)  
+
+# Not equal to
+print(x != y)  
+
+# Greater than
+print(x > y)  
+
+# Less than
+print(x < y) 
+
+# Greater than or equal to
+print(x >= 5)  
+
+# Less than or equal to
+print(y <= 3)  
