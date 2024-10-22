b853a258e950728828ce37eb487ad5cdb3ed8840
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-22 21:19:11 +0530

    Create 4.1 Conditional Statements.py

---
+++ 4.1 Conditional Statements.py
@@ -0,0 +1,15 @@
+## 4.1 Conditional Statements (if, elif, else)
+
+# Conditional statements allow you to run different blocks of code based on certain conditions. 
+# In Python, the primary conditional statements are if, elif, and else.
+
+'''
+Syntax : 
+
+if condition:
+    # Code block to execute if condition is True
+elif another_condition:
+    # Code block to execute if the first condition is False, but this one is True
+else:
+    # Code block to execute if all previous conditions are False
+'''
