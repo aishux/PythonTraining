189aca2263fb13b8065aced4d051af80cd3bb539
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-22 19:18:18 +0530

    Create 2.1 Comments&Docstrings.py

---
+++ 2.1 Comments&Docstrings.py
@@ -0,0 +1,15 @@
+## 2.1 Comments and Docstrings
+
+# Comments and docstrings are used to make your code more understandable for yourself and others. 
+# Python ignores comments when running code.
+
+
+# Comments: Single-line comments start with a #.
+print("Hello, World!")  # This prints a message
+
+
+# Docstrings: These are multi-line comments that are enclosed in triple quotes (""" """).
+
+"""
+This program prints a greeting message.
+"""
