7d7f389f47abf0cd6a1d23afb31d08e685ab2f2d
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-22 20:50:07 +0530

    Create 3.1 Arithmetic Operators.py

---
+++ 3.1 Arithmetic Operators.py
@@ -0,0 +1,28 @@
+## 3.1 Arithmetic Operators
+# Arithmetic operators are used to perform basic mathematical operations like addition, subtraction, multiplication, etc.
+
+#Examples:
+
+a = 10
+b = 5
+
+# Addition
+sum_result = a + b  # 15
+
+# Subtraction
+diff_result = a - b  # 5
+
+# Multiplication
+prod_result = a * b  # 50
+
+# Division
+div_result = a / b  # 2.0 (float)
+
+# Floor Division
+floor_div_result = a // b  # 2
+
+# Modulus (remainder)
+mod_result = a % b  # 0
+
+# Exponentiation
+exp_result = a ** 2  # 100 (10 to the power of 2)
