239724b9b7c97ed374b5642d0a3707946fbfcef0
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-22 21:08:25 +0530

    Create 3.4 Logical Operators.py

---
+++ 3.4 Logical Operators.py
@@ -0,0 +1,13 @@
+## 3.4 Logical Operators
+#Logical operators combine conditional statements and return True or False based on the logic applied.
+
+# Using 'and'
+a = 5
+b = 10
+print(a > 3 and b > 3)  
+
+# Using 'or'
+print(a > 10 or b > 3)  
+
+# Using 'not'
+print(not (a > 3))  
