3d4816c8f4fb7a7ecae5ba5f21ccd068d88dfcf4
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-22 20:15:28 +0530

    Create Basic Input Output.py

---
+++ Basic Input Output.py
@@ -0,0 +1,12 @@
+## 2.4 Basic Input/Output (print() and input())
+
+
+# print() Function: Used to display output.
+# input() Function: Used to take user input. The input is always treated as a string.
+
+
+name = input("Enter your name: ")       # Asks for user input
+print("Hello, ", name)                 
+
+
+# Exercise: Print your age 
