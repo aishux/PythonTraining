50aad1a2d28e9a68bf5eda68c6a688cc80ffc919
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-22 19:48:47 +0530

    Create 2.3 Type Casting.py

---
+++ 2.3 Type Casting.py
@@ -0,0 +1,17 @@
+## 2.3 Type Casting
+
+#Sometimes, you need to convert one data type to another. This is called type casting. 
+# For example, you might want to turn an integer into a string or vice versa.
+
+
+# Converting to String:
+age = 25
+age_str = str(age)  # Convert integer to string
+print("I am " + age_str + " years old.")  
+
+
+# Converting to Integer:
+selling_price = "100"
+cost_price = 50
+selling_price_int = int(selling_price)  # Convert string to integer
+print("Profit = Rs", selling_price_int-cost_price)
