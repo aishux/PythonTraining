c73f77c6ed18bdabad1a53872f5cbe07918e0ab9
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-23 22:12:33 +0530

    Create 7.1 String Indexing and Slicing.py

---
+++ 7.1 String Indexing and Slicing.py
@@ -0,0 +1,11 @@
+## 7.1 String Indexing and Slicing
+# Indexing: Access individual characters in a string using their position (starting from 0).
+my_string = "Hello"
+print(my_string[0])  
+print(my_string[-1])   # negative indexing starts from the end
+
+
+#Slicing: Extract parts of a string using start and end indices.
+print(my_string[1:4])   # start index inclusive, end index exclusive
+print(my_string[:3])   
+print(my_string[2:])   
