0e4f06aeab3c0f7f10130bc56c76505154a5389a
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-23 22:14:20 +0530

    Create 7.2 String Methods.py

---
+++ 7.2 String Methods.py
@@ -0,0 +1,18 @@
+## 7.2 String Methods
+# Python provides several built-in methods to manipulate strings:
+
+# upper(): Converts the string to uppercase.
+print(my_string.upper())  
+
+
+# lower(): Converts the string to lowercase.
+print(my_string.lower())  
+
+
+# replace(old, new): Replaces part of a string with another.
+print(my_string.replace("H", "J"))  
+
+
+# strip(): Removes whitespace from the beginning and end of the string.
+another_string = "  Hello  "
+print(another_string.strip())  
