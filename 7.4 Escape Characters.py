c41cd498251a0402c71e970558a98fcf0c1bd083
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-23 22:19:19 +0530

    Create 7.4 Escape Characters.py

---
+++ 7.4 Escape Characters.py
@@ -0,0 +1,13 @@
+## 7.4 Escape Characters
+# Escape characters allow you to include special characters in strings.
+
+# \n: Newline.
+print("Hello\nWorld")  
+
+
+# \t: Tab.
+print("Hello\tWorld")  
+
+
+#\\: Backslash.
+print("This is a backslash: \\")  
